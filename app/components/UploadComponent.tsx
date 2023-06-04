a5c3b9b463bf1b3cbea10559202c56331ce66117
kirandash <thekirankumardash@gmail.com>
2023-06-04 10:48:19 +0800

    feat: add side by side and slider compare tool

--- app/components/UploadComponent.tsx
+++ app/components/UploadComponent.tsx
@@ -5,6 +5,8 @@ import { useState } from 'react'
 import { UploadDropzone } from 'react-uploader'
 import { Uploader } from 'uploader'
 import { downloadImg } from '../../utils/downloadImg'
+import CompareSlider from './CompareSlider'
+import Toggle from './Toggle'
 
 if (!process.env.NEXT_PUBLIC_UPLOAD_IO_API_KEY)
   throw new Error('UPLOAD_IO_API_KEY is not set')
@@ -45,6 +47,7 @@ export default function UploadComponent() {
   const [restoredImageUrl, setRestoredImageUrl] = useState<string | null>(null)
   const [restoredImageLoaded, setRestoredImageLoaded] = useState(false)
   const [downloading, setDownloading] = useState(false)
+  const [sidebyside, setSidebyside] = useState(true)
 
   async function restoreImage(imageUrl: string) {
     try {
@@ -77,35 +80,59 @@ export default function UploadComponent() {
 
   return (
     <>
-      <UploadDropzone
-        uploader={uploader}
-        options={uploaderOptions}
-        onUpdate={(files) => {
-          if (files.length === 0) {
-            console.log('No files selected.')
-          } else {
-            console.log('File selected: ', files[0].fileUrl)
-            setImageUrl(files[0].fileUrl)
-            restoreImage(files[0].fileUrl)
-          }
-        }}
-        // onComplete={(files) => alert(files.map((x) => x.fileUrl).join('\n'))}
-        width="600px"
-        height="375px"
-      />
-      <div className="flex gap-10">
-        {/* https://upcdn.io/12a1yJB/raw/uploads/2023/06/04/PASSPORT_PHOTO-5wmg.jpg */}
-        {imageUrl && (
-          <Image src={imageUrl} alt="Main image" width={400} height={400} />
-        )}
-        {restoredImageUrl && (
-          <Image
-            src={restoredImageUrl}
-            alt="Restored image"
-            width={400}
-            height={400}
-            onLoad={() => setRestoredImageLoaded(true)}
+      {imageUrl && restoredImageUrl && (
+        <>
+          <Toggle
+            enabled={sidebyside}
+            setEnabled={(val) => setSidebyside(val)}
           />
+          {!sidebyside && (
+            <CompareSlider
+              original={imageUrl}
+              restored={restoredImageUrl}
+              classNames="w-[400px]"
+              portrait={true}
+            />
+          )}
+        </>
+      )}
+      {!imageUrl && (
+        <UploadDropzone
+          uploader={uploader}
+          options={uploaderOptions}
+          onUpdate={(files) => {
+            if (files.length === 0) {
+              console.log('No files selected.')
+            } else {
+              console.log('File selected: ', files[0].fileUrl)
+              setImageUrl(files[0].fileUrl)
+              restoreImage(files[0].fileUrl)
+            }
+          }}
+          // onComplete={(files) => alert(files.map((x) => x.fileUrl).join('\n'))}
+          width="600px"
+          height="375px"
+        />
+      )}
+      <div className="flex gap-10">
+        {sidebyside && (
+          <>
+            {/* https://upcdn.io/12a1yJB/raw/uploads/2023/06/04/PASSPORT_PHOTO-5wmg.jpg */}
+            {imageUrl && (
+              <Image src={imageUrl} alt="Main image" width={400} height={400} />
+            )}
+            {restoredImageUrl && (
+              <a href={restoredImageUrl} target="_blank" rel="noreferrer">
+                <Image
+                  src={restoredImageUrl}
+                  alt="Restored image"
+                  width={400}
+                  height={400}
+                  onLoad={() => setRestoredImageLoaded(true)}
+                />
+              </a>
+            )}
+          </>
         )}
         {restoredImageLoaded && (
           <button onClick={() => downloadRestoredImg}>
