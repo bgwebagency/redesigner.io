23266ad96d5bbe639238b227e23570d95fcdf3ed
kirandash <thekirankumardash@gmail.com>
2023-06-20 01:42:53 +0800

    fix: merge conflict

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,23 @@
-"use client";
-import Image from "next/image";
-import { Button } from "greenhouse-react-ui";
-import { useRouter } from "next/navigation";
-import { useMediaQueries } from "@react-hook/media-query";
-import React from "react";
-import Loading from "./components/Loading";
+'use client'
+import Image from 'next/image'
+import { Button } from 'greenhouse-react-ui'
+import { useRouter } from 'next/navigation'
+import { useMediaQueries } from '@react-hook/media-query'
+import React from 'react'
+import Loading from './components/Loading'
 
 export default function Home() {
-  const router = useRouter();
+  const router = useRouter()
   const { matches } = useMediaQueries({
-    md: "only screen and (min-width: 768px)",
-    xxl: "only screen and (min-width: 1536px)",
-  });
-  const [isLoading, setLoading] = React.useState(false);
+    md: 'only screen and (min-width: 768px)',
+    xxl: 'only screen and (min-width: 1536px)',
+  })
+  const [isLoading, setLoading] = React.useState(false)
 
   return (
     <div className="mb-auto">
       <div className="flex flex-col justify-center mt-8 md:mt-14 2xl:mt-40">
-        <h1
-          className="text-3xl md:text-5xl 2xl:text-6xl font-bold text-center leading-normal 2xl:leading-tight"
-        >
+        <h1 className="text-3xl md:text-5xl 2xl:text-6xl font-bold text-center leading-normal 2xl:leading-tight">
           Redesign your home <br />
           <span className="text-primary">with AI</span>
         </h1>
@@ -30,7 +28,7 @@ export default function Home() {
           <a
             href="https://youtu.be/4YXUGuo9OM4"
             target="_blank"
-            rel={"noopener"}
+            rel={'noopener'}
             className="underline tracking-wider"
           >
             Learn How I built this
@@ -40,8 +38,8 @@ export default function Home() {
           <div className="text-center mt-6">
             <Button
               onClick={() => {
-                setLoading(true);
-                router.push("/room");
+                setLoading(true)
+                router.push('/room')
               }}
               className="tracking-wider text-xs md:text-sm"
             >
@@ -51,8 +49,8 @@ export default function Home() {
           <div className="text-center mt-6">
             <Button
               onClick={() => {
-                setLoading(true);
-                router.push("/building");
+                setLoading(true)
+                router.push('/building')
               }}
               layout="outline"
               className="tracking-wider text-xs md:text-sm"
@@ -65,7 +63,7 @@ export default function Home() {
       <div className="flex justify-center gap-6 mt-10 md:mt-14 2xl:mt-20">
         <div className="bg-white rounded-2xl p-2 2xl:p-4">
           <Image
-            src={"/input.jpg"}
+            src={'/input.jpg'}
             alt="Original bedroom"
             width={matches.xxl ? 400 : matches.md ? 250 : 300}
             height={matches.xxl ? 400 : matches.md ? 250 : 300}
@@ -73,7 +71,7 @@ export default function Home() {
         </div>
         <div className="bg-white rounded-2xl p-2 2xl:p-4">
           <Image
-            src={"/input-predicted.jpg"}
+            src={'/input-predicted.jpg'}
             alt="Predicted bedroom"
             width={matches.xxl ? 400 : matches.md ? 250 : 300}
             height={matches.xxl ? 400 : matches.md ? 250 : 300}
@@ -82,5 +80,5 @@ export default function Home() {
       </div>
       {isLoading && <Loading />}
     </div>
-  );
+  )
 }
