625f2a6a1a747d2293b41d656253ccfce1f1e173
Lautaro <lausuarez71@gmail.com>
2023-06-19 15:39:55 +0200

    Fix: fix typo on test

--- app/components/Header/__test__/Header.test.tsx
+++ app/components/Header/__test__/Header.test.tsx
@@ -7,7 +7,7 @@ describe('Header test', () => {
     it('Render the right way the Header"', () => {
       render(<Header/>);
       const aTag = screen.getByLabelText('Github')
-      expect(aTag).toBeInTheDocument
+      expect(aTag).toBeInTheDocument()
       expect(screen.getByText('redesigner.io')).toBeInTheDocument();
     });
 });
