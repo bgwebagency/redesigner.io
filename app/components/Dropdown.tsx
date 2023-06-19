c643ff99334da35d8fc0ade41e66af14e1e95f42
kirandash <thekirankumardash@gmail.com>
2023-06-20 01:57:17 +0800

    fix: dropdown color for white theme

--- app/components/Dropdown.tsx
+++ app/components/Dropdown.tsx
@@ -29,10 +29,10 @@ export default function Dropdown({
     <div className={clsx(className, 'w-56')} {...props}>
       <Menu as="div" className="relative inline-block text-left">
         <div>
-          <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 border">
+          <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium hover:bg-opacity-30 focus:outline-none focus-visible:ring-2  focus-visible:ring-opacity-75 border focus-visible:ring-white dark:border-white border-gray-950">
             {selected ?? label}
             <ChevronDownIcon
-              className="ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100"
+              className="ml-2 -mr-1 h-5 w-5"
               aria-hidden="true"
             />
           </Menu.Button>
