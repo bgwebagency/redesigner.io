adf96ed8b6306cf9b8fff5fb88807cb847e00ce0
Lautaro Suarez <67484166+lausuarez02@users.noreply.github.com>
2023-06-21 10:32:32 +0200

    Update ThemeSelector.tsx

--- app/components/ThemeSelector.tsx
+++ app/components/ThemeSelector.tsx
@@ -35,14 +35,13 @@ const ThemeSelector = ({
       {options.map((item) => (
         <button
           key={item}
-          className={clsx(
-            'border rounded-lg px-4 py-2 text-xs md:text-sm',
-            item === selected && 'border-primary text-primary'
-          )}
+         className={clsx('flex flex-col items-center justify-center border rounded-lg px-4 py-2 text-xs md:text-sm',
+			item === selected && 'border-primary text-primary',)}
+		
           onClick={() => onChange(item)}
         >
           <Image alt={pathname} src={ pathname === '/room' ? `${pathname}-${item.toLowerCase()}.jpg` : `${pathname}-${item.toLowerCase()}.png` } loading="lazy" width={100}
-        height={100}/>
+        height={100} className="mb-2 mx-auto"/>
           {item}
         </button>
       ))}
