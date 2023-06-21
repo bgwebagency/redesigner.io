52d82e1345dc555421004a94a457ddcf9d6bfbd6
kirandash <thekirankumardash@gmail.com>
2023-06-21 20:11:42 +0800

    docs: readme update

--- app/components/ThemeSelector.tsx
+++ app/components/ThemeSelector.tsx
@@ -21,32 +21,43 @@ const ThemeSelector = ({
 	onChange,
 	...props
 }: Props) => {
-  //Get the path
-  const pathname = usePathname()
+	//Get the path
+	const pathname = usePathname()
 
-  return (
-    <div
-      className={clsx(
-        'grid grid-cols-3 md:grid-cols-5 gap-2 md:gap-6',
-        className
-      )}
-      {...props}
-    >
-      {options.map((item) => (
-        <button
-          key={item}
-         className={clsx('flex flex-col items-center justify-center border rounded-lg px-4 py-2 text-xs md:text-sm',
-			item === selected && 'border-primary text-primary',)}
-		
-          onClick={() => onChange(item)}
-        >
-          <Image alt={pathname} src={ pathname === '/room' ? `${pathname}-${item.toLowerCase()}.jpg` : `${pathname}-${item.toLowerCase()}.png` } loading="lazy" width={100}
-        height={100} className="mb-2 mx-auto"/>
-          {item}
-        </button>
-      ))}
-    </div>
-  )
+	return (
+		<div
+			className={clsx(
+				'grid grid-cols-3 md:grid-cols-5 gap-2 md:gap-6',
+				className,
+			)}
+			{...props}
+		>
+			{options.map(item => (
+				<button
+					key={item}
+					className={clsx(
+						'flex flex-col items-center justify-center border rounded-lg px-4 py-2 text-xs md:text-sm',
+						item === selected && 'border-primary text-primary',
+					)}
+					onClick={() => onChange(item)}
+				>
+					<Image
+						alt={pathname}
+						src={
+							pathname === '/room'
+								? `${pathname}-${item.toLowerCase()}.jpg`
+								: `${pathname}-${item.toLowerCase()}.png`
+						}
+						loading="lazy"
+						width={100}
+						height={100}
+						className="mb-2 mx-auto"
+					/>
+					{item}
+				</button>
+			))}
+		</div>
+	)
 }
 
 export default ThemeSelector
